05eec94ec5cd0cc17f479227071541ac22a4fd98
pesp1 <alexvana2006@gmail.com>
2025-04-05 17:50:07 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,9 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyB0vdTPJEeqayeO8BbkFOpMMshhqjYSVWU",
+  authDomain: "clickergame-10ffb.firebaseapp.com",
+  projectId: "clickergame-10ffb",
+  storageBucket: "clickergame-10ffb.firebasestorage.app",
+  messagingSenderId: "584494192644",
+  appId: "1:584494192644:web:f42204feecd0a32b18fd51",
+  measurementId: "G-B0KTFX5JJH"
+};
